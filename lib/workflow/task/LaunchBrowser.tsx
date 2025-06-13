40e4f889348847a14ebd404ad5217fd1e050a565
Priyanshu7798 <124646393+Priyanshu7798@users.noreply.github.com>
2025-06-13 12:51:59 +0530

    Worflow using React Flow created

---
+++ lib/workflow/task/LaunchBrowser.tsx
@@ -0,0 +1,21 @@
+import { TaskType } from "@/types/task";
+import { GlobeIcon, LucideProps } from "lucide-react";
+
+
+export const LaunchBrowserTask = {
+  type: TaskType.LAUNCH_BROWSER,
+  label : "Launch Browser",
+  icon : (props: LucideProps)=>(
+    <GlobeIcon className="stroke-pink-400" {...props} />
+  ),
+  isEntryPoint :true,
+  inputs:[
+    {
+      name : "Website URL",
+      type : "STRING",
+      helperText : "eg : https://www.google.com",
+      required: true,
+      hideHande : true,
+    }
+  ]
+};
---
+++ lib/workflow/task/registry.tsx
@@ -0,0 +1,7 @@
+import { TaskType } from "@/types/task";
+import { LaunchBrowserTask } from "./LaunchBrowser";
+
+
+export const TaskRegistry = {
+  [TaskType.LAUNCH_BROWSER]: LaunchBrowserTask,
+}
